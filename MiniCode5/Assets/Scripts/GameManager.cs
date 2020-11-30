a8630eacf3590f3bd7c8fbc3db74f2bc1336e17a
Junhao <tjunhao17@gmail.com>
2020-11-30 16:05:37 +0800

    version 1

---
+++ MiniCode5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float levelTime;
+    public GameObject addEnergyPrefab;
+    public GameObject minusEnergyPrefab;
+    public GameObject TimeLimit;
+    public int numberOfSpawn;
+    public static GameManager instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < numberOfSpawn; i++)
+        {
+            Vector3 randomPos = new Vector3(Random.Range(-15, 15), 0, Random.Range(-15, 15));
+            if(Random.Range(0,2)<1)
+            {
+                Instantiate(addEnergyPrefab, randomPos, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(minusEnergyPrefab, randomPos, Quaternion.identity);
+            }
+        }
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(levelTime>0)
+        {
+            levelTime -= Time.deltaTime;
+            //print("levelTime: " + levelTime);
+            print("LevelTime: " + FormatTime(levelTime));
+            TimeLimit.GetComponent<Text>().text = "LevelTime: " + FormatTime(levelTime);
+        }
+        else
+        {
+            levelTime = 0;
+            print("Times up");
+        }
+        if (GameManager.instance.levelTime < 0)
+        {
+            TimeLimit.GetComponent<Text>().text = "LevelTime: 00:00:00 ";
+        }
+    }
+    public string FormatTime(float time)
+    {
+        int minutes = (int)time / 60;
+        int seconds = (int)time - 60 * minutes;
+        int milliseconds = (int)(1000 * (time - minutes * 60 - seconds));
+        return string.Format("{0:00} : {1:00} : {2:00}", minutes, seconds, milliseconds);
+    }
+    public void spawn ()
+    {
+        for (int i = 0; i < numberOfSpawn; i++)
+        {
+            Vector3 randomPos = new Vector3(Random.Range(-15, 15), 0, Random.Range(-15, 15));
+            if (Random.Range(0, 2) < 1)
+            {
+                Instantiate(addEnergyPrefab, randomPos, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(minusEnergyPrefab, randomPos, Quaternion.identity);
+            }
+        }
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+}
---
+++ MiniCode5/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    float EnergyCount;
+    public GameObject EnergyText;
+    public Animator playerAni;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.instance.levelTime > 0)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                playerAni.SetBool("Walk", true);
+            }
+            else if (Input.GetKeyUp(KeyCode.W))
+            {
+                playerAni.SetBool("Walk", false);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                playerAni.SetBool("Walk", true);
+            }
+            else if (Input.GetKeyUp(KeyCode.S))
+            {
+                playerAni.SetBool("Walk", false);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, -90, 0);
+                playerAni.SetBool("Walk", true);
+            }
+            else if (Input.GetKeyUp(KeyCode.A))
+            {
+                playerAni.SetBool("Walk", false);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, 90, 0);
+                playerAni.SetBool("Walk", true);
+            }
+            else if (Input.GetKeyUp(KeyCode.D))
+            {
+                playerAni.SetBool("Walk", false);
+            }
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S))
+            {
+                playerAni.SetBool("Walk", true);
+            }
+        }
+        else if (GameManager.instance.levelTime < 0)
+        {
+            playerAni.SetBool("Walk", false);
+            SceneManager.LoadScene("LoseScene");
+        }
+        if(EnergyCount <0)
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
+        if(EnergyCount>50)
+        {
+            SceneManager.LoadScene("WinScene");
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Add"))
+        {
+            Destroy(collision.gameObject);
+            EnergyCount = EnergyCount + 5;
+            EnergyText.GetComponent<Text>().text = "EnergyCount: " + EnergyCount;
+            GameManager.instance.levelTime += (5);
+            GameManager.instance.spawn();
+        }
+        if(collision.gameObject.CompareTag("Minus"))
+        {
+            Destroy(collision.gameObject);
+            EnergyCount = EnergyCount - 25;
+            EnergyText.GetComponent<Text>().text = "EnergyCount: " + EnergyCount;
+        }
+    }
+}
